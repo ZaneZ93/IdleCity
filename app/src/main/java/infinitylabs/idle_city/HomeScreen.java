8cb71b68a599536e4ba8d96c4121439721470dd2
ZaneZ93 <35323560+ZaneZ93@users.noreply.github.com>
2018-02-14 14:54:27 -0800

    Initial commit

---
+++ app/src/main/java/infinitylabs/idle_city/HomeScreen.java
@@ -0,0 +1,16 @@
+package infinitylabs.idle_city;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+    public void openSettime(View view) {
+    }
+}
---
+++ app/src/main/java/infinitylabs/idle_city/setTime.java
@@ -0,0 +1,13 @@
+package infinitylabs.idle_city;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class setTime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_time);
+    }
+}
